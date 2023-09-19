c3c6068e0534e7a3c2bee5c582da6a13ec1e0b94
김종혁 <notify9637@naver.com>
2023-09-19 10:56:14 +0900

    aoptest

--- emaillist03/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
@@ -1,21 +1,37 @@
 package com.poscodx.emaillist.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.poscodx.emaillist.repository.EmailListRepository;
+import com.poscodx.emaillist.vo.EmailListVo;
 
 @Controller
 public class EmaillistController {
 	
 	@Autowired // Root Application context에서 wire 해옴
 	private EmailListRepository emailListRepository;
-	@ResponseBody
 	@RequestMapping("/")
-	public String main() {
-		emailListRepository.findAll();
-		return "ok!";
+	public String main(Model model) {
+		List<EmailListVo> list = emailListRepository.findAll();
+		model.addAttribute("list", list);
+		return "index";
 	}
+	
+	@RequestMapping(value="/add",method=RequestMethod.GET)
+	public String add() {
+		return "add";
+	}
+	
+	@RequestMapping(value="/add",method=RequestMethod.POST)
+	public String add(EmailListVo vo) {
+		emailListRepository.insert(vo);
+		return "redirect:/";
+	}
+	
 }
